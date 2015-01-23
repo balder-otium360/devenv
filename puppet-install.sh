a23f9d177bcef464253fa09502f4e6ace2641a08
Bálder Carraté <balder.carrate@otium360.com>
2015-01-23 15:38:14 +0100

    Update puppet-install.sh

--- puppet-install.sh
+++ puppet-install.sh
@@ -7,14 +7,14 @@ echo "    Current: $CURRENT_VERSION"
 echo "    Target: $TARGET_VERSION"
 
 #if command -v puppet >/dev/null 2>&1; then
-if [ "$CURRENT_VERSION" == "$TARGET_VERSION" ]; then
-  echo "Puppet is up to date"
-else
+if [[ $CURRENT_VERSION < $TARGET_VERSION ]]; then
   echo "Installing Puppet $TARGET_VERSION..."
   wget --quiet -O /tmp/puppetlabs-release-trusty.deb https://apt.puppetlabs.com/puppetlabs-release-trusty.deb
   sudo dpkg -i /tmp/puppetlabs-release-trusty.deb
   sudo apt-get update -qq
   sudo apt-get install -qq puppet
+else
+  echo "Puppet is up to date"
 fi
 
 echo "Installing Puppet Modules..."
